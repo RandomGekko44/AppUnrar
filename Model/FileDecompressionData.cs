e7c76232d6f69528459a3a9361c40af96bd63f39
Eduardo C. <82246362+RandomGekko44@users.noreply.github.com>
2024-08-26 21:35:33 -0600

    Add files via upload

---
+++ Model/FileDecompressionData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUnrar.Model
+{
+    public class FileDecompressionData
+    {
+        public File? fileData { get; set; }
+        public string? output_path { get; set; }
+        public System.Diagnostics.Process? decompressionProcess { get; set; }
+    }
+}
